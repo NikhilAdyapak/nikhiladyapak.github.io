c41fa8739a33d68bfb91df0ab09f864ead2cdff7
Nikhil Adyapak <nikhiladyapak31@gmail.com>
2026-02-13 14:18:49 -0600

    updated website 3.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 const sr = ScrollReveal({
     origin: 'bottom',
-    distance: '30px',
-    duration: 800,
+    distance: '20px',
+    duration: 1000,
     delay: 100,
     reset: false
 });
 
-sr.reveal('.hero-content', {});
+sr.reveal('.hero-content');
 sr.reveal('.about-grid', { delay: 200 });
 sr.reveal('.timeline-item', { interval: 100 });
 sr.reveal('.project-card', { interval: 100 });
 sr.reveal('.comm-card', { interval: 100 });
+sr.reveal('.badge-row', { interval: 100 });
